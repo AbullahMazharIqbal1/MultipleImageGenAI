305122e19ff499eb3bbbe712037e0099fca257ab
AbullahMazharIqbal1 <abdullahmazhar4578@gmail.com>
2025-09-29 16:25:51 +0500

    main changes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/ImageGenAI/" : "/",
   server: {
     host: "::",
     port: 8080,
